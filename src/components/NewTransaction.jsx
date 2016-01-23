cbe384b2eef60859390c7c223f8fbd500d6c73f4
Bernardo Garciarivas <brgarciarivas@gmail.com>
2016-01-23 13:53:02 -0500

    NewTransaction

---
+++ src/components/NewTransaction.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import base from './Base'
+
+import LeftNave from 'material-ui/lib/left-nav'
+import MenuItem from 'material-ui/lib/menus/menu-item'
+import RaisedButton from 'material-ui/lib/raised-button'
+
+
+class NewTransaction extends Base {
+	constructor(props){
+		super(props)
+		this.autoBind()
+	}
+}
+	
+
