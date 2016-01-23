a80da0be7ff6eb78ff7b2e96756b40378d82fff2
jciancio <jciancio7@gmail.com>
2016-01-23 15:59:34 -0500

    sending balance amount & currency & account name to /getBalances as balance object

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 var express = require('express');
 var app = express();
-var User = require('./models/User');
+// var User = require('./models/User');
 var coinbase = require('coinbase');
 var settings = require('./config/settings');
 
 var client = new coinbase.Client({
     'apiKey': settings.API_KEY,
-    'apiSecret': settings.API_SECRET
+    'apiSecret': settings.API_SECRET,
+    'baseApiUri': 'https://api.sandbox.coinbase.com/v2/',
+    'tokenUri': 'https://api.sandbox.coinbase.com/oauth/token'
 });
 
 app.get('/getBalance', function (req, res) {
-    console.log('Got a request!');
-
-    res.send('Hey thanks for the request.');
+  console.log('Your balance is...');
+  client.getAccounts({}, function(err, accounts) {
+    console.log('may bal: ' +  accounts[1].balance.amount + ' for ' + accounts[1].name);
+    var balance = new Object();
+    balance.amount = accounts[1].balance.amount;
+    balance.currency = accounts[1].balance.currency;
+    balance.name = accounts[1].name;
+    res.send(balance);
+  });
 });
 
 app.get('/getAccount', function (req, res) {
