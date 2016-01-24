f683b4daf31d5c82011190950b5c05b95e669464
krefsar <evanm.design@gmail.com>
2016-01-24 07:33:58 -0500

    remove extraneous files

--- config/settings.js
+++ config/settings.js
@@ -1,8 +1,3 @@
-// module.exports = {
-//     API_KEY: 'FdT4nLWMePVEUjuk',
-//     API_SECRET: 'wKLByFqLLM6zhSac3JCElDulL14o5IaD'
-// }
-
 module.exports = {
     API_KEY: 'ltkf7QPvJrT900MQ',
     API_SECRET: 'Z20huhqiOKOv64CQGBiIrpkR1m4I4JQf'
